56a0eb6c2afea928bf24f383ce55030b4f8f9bef
katrpilar <kbrinso@gmail.com>
2018-04-30 11:17:56 -0700

    create and show company implemented

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,25 @@
 class CompaniesController < ApplicationController
 	  before_action :authenticate_user!
 
+	  def new
+	  	@company = Company.new
+	  end
+
+
+	  def create
+	  	@company = Company.new(params.require(:company).permit(:name))
+	    @company.save
+	    redirect_to company_path(@company)
+	  end
+
+	  def show
+	  	@company = Company.find(params[:id])
+	  end
+
+	  def update
+	    @company = Company.find(params[:id])
+	    @company.update(params.require(:name))
+	    redirect_to company_path(@company)
+	  end
+
 end
