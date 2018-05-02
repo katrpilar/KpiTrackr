55bfc58286cce088dfb531994a6c15bbe3fe8265
katrpilar <kbrinso@gmail.com>
2018-05-02 12:58:44 -0700

    changing routes to nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
-  resources :actual_metric_updates
-  resources :company_kpis
-  resources :companies
+#   resources :actual_metric_updates
+
+	# scope path: ":company_kpi_id", as: "kpi" do
+	#   resources :projects
+	# end
+  
+	# map.resources :companies do |companies|
+	# 	companies.resources :company_kpis, :name_prefix => "kpi_"
+	# end
+
+	# map.resources :company_kpis do |company_kpis|
+	# 	company_kpis.resources :actual_metric_updates, :name_prefix => "add_"
+	# end
+
+  resources :companies do
+  	resources :company_kpis
+  end
+
+  resources :company_kpis do
+  	resources :actual_metric_updates, as: 'metrics'
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
