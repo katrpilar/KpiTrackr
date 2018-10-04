b2614200bcdcec062cd6d0b2054f325d76b1337c
katrpilar <kbrinso@gmail.com>
2018-10-04 14:24:16 -0700

    Abstracted Kpi controller actions

--- app/controllers/kpis_controller.rb
+++ app/controllers/kpis_controller.rb
@@ -29,16 +29,7 @@ class KpisController < ApplicationController
 
 		if @kpi.valid?
 			@kpi.save
-			case @kpi.kpiable_type
-			when "Company"				
-				redirect_to company_path(current_user.company)
-			when "Team"
-				redirect_to team_path(Team.find(@kpi.kpiable_id))
-			when "Member"
-				redirect_to member_path(Member.find(@kpi.kpiable_id))
-			else
-				redirect_to :root_path
-			end
+			kpiable_routes
 		else 
 			render :new
 		end
@@ -53,16 +44,7 @@ class KpisController < ApplicationController
 		
 		@company = current_user.company
 		if @kpi.update(params.require(:kpi).permit(:name, :unit, :target, :target_start_date, :target_end_date))
-			case @kpi.kpiable_type
-			when "Company"				
-				redirect_to company_path(current_user.company)
-			when "Team"
-				redirect_to team_path(Team.find(@kpi.kpiable_id))
-			when "Member"
-				redirect_to member_path(Member.find(@kpi.kpiable_id))
-			else
-				redirect_to :root_path
-			end
+			kpiable_routes
 		else
 			render :edit
 		end
@@ -72,35 +54,40 @@ class KpisController < ApplicationController
 	def destroy
 		@kpi = Kpi.find(params[:id])
 		@kpi.destroy
-		case @kpi.kpiable_type
-			when "Company"				
-				redirect_to company_path(current_user.company)
-			when "Team"
-				redirect_to team_path(Team.find(@kpi.kpiable_id))
-			when "Member"
-				redirect_to member_path(Member.find(@kpi.kpiable_id))
-			else
-				redirect_to :root_path
-			end
+		kpiable_routes
 	end
 
 	private 
-       def set_kpiable
-       		if params.has_key?(:company_id)
-       			@kpiable = Company.find(params[:company_id])
-       		elsif params.has_key?(:team_id)
-       			@kpiable = Team.find(params[:team_id])
-       		elsif params.has_key?(:member_id)
-       			@kpiable = Member.find(params[:member_id])
-       		end
-       end
-       def set_kpi_instance
-       	if params.has_key?(:company_id)
-					@company = Company.find(params[:company_id])
-				elsif params.has_key?(:team_id)
-					@company = Team.find(params[:team_id]).company
-				elsif params.has_key?(:member_id)
-					@company = Member.find(params[:member_id]).team.company
-				end
-       end
+   def set_kpiable
+   		if params.has_key?(:company_id)
+   			@kpiable = Company.find(params[:company_id])
+   		elsif params.has_key?(:team_id)
+   			@kpiable = Team.find(params[:team_id])
+   		elsif params.has_key?(:member_id)
+   			@kpiable = Member.find(params[:member_id])
+   		end
+   end
+
+   def set_kpi_instance
+   	if params.has_key?(:company_id)
+			@company = Company.find(params[:company_id])
+		elsif params.has_key?(:team_id)
+			@company = Team.find(params[:team_id]).company
+		elsif params.has_key?(:member_id)
+			@company = Member.find(params[:member_id]).team.company
+		end
+   end
+
+   def kpiable_routes
+ 		case @kpi.kpiable_type
+		when "Company"				
+			redirect_to company_path(current_user.company)
+		when "Team"
+			redirect_to team_path(Team.find(@kpi.kpiable_id))
+		when "Member"
+			redirect_to member_path(Member.find(@kpi.kpiable_id))
+		else
+			redirect_to :root_path
+		end
+   end    
 end
